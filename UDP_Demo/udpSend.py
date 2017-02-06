6b7d471411a3112296cad923b81bd68d40f074fd
liuyang0618 <ly_btest@126.com>
2017-02-06 16:09:00 +0800

    add folder

--- UDP_Demo/udpSend.py
+++ UDP_Demo/udpSend.py
@@ -10,7 +10,7 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # 指定地址和端口 & 发送消息
-s.sendto("Hello  world", ("192.168.98.213", 5055))
+s.sendto("Hello world", ("192.168.98.213", 5055))
 
 #关闭连接
 s.close()
