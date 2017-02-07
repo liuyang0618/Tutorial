41e27b2b7c461fb43ab171af2254561d9ed55977
liuyang0618 <ly_btest@126.com>
2017-02-07 17:34:21 +0800

    wxPython

---
+++ wxPython_Demo/catchPosition.py
@@ -0,0 +1,41 @@
+#encoding:utf-8
+__author__ = 'Administrator'
+
+"""
+需求：
+捕获窗体坐标
+    创建一个窗体，该窗体中有一个文本框，当鼠标在窗体中移动时，文本框显示当前鼠标的位置
+"""
+
+import wx
+
+#创建框架类
+class myFrame(wx.Frame):
+    def __init__(self,superior):
+        wx.Frame.__init__(self,parent=superior,title=u'第一个窗体',pos =(100, 200),  size=(300,300))
+        #创建一个面板
+        panel = wx.Panel(self)
+        #将鼠标移动的事件绑定到自定义的OnMove方法
+        panel.Bind(wx.EVT_MOTION, self.onMove)
+        panel.Bind
+        #显示一个静态文本框
+        wx.StaticText(parent=panel, label="Pos:", pos=(10,20))
+        #创建一个文本框输入框
+        self.posCtrl = wx.TextCtrl(parent=panel, pos=(40,20))
+
+    def onMove(self, event):
+        #获取事件的位置
+        pos = event.GetPosition()
+        #给文本输入框添加内容
+        self.posCtrl.SetValue("%s, %s" % (pos.x, pos.y))
+
+if __name__ == "__main__":
+    #定义应用程序类的一个对象
+    app = wx.App()
+    #创建框架对象
+    frame = myFrame(None)
+    #框架显示
+    frame.Show(True)
+    #创建事物循环
+    app.MainLoop()
+
--- wxPython_Demo/wx_helloworld.py
+++ wxPython_Demo/wx_helloworld.py
@@ -1,11 +1,19 @@
+#encoding:utf-8
 __author__ = 'Administrator'
 
+#导入 wx 模块
 import wx
 
+#定义应用程序类的一个对象
 app = wx.App()
-window = wx.Frame(None, title = "wxPython - www.yiibai.com", size = (400,300))
+#创建一个顶层窗口的 wx.Frame 类的对象。 给出构造标题和尺寸参数。
+window = wx.Frame(None, title = "wxPython - http://www.yiibai.com/", size = (400,300))
+#虽然其他控件可以在Frame对象加入，但它们的布局无法管理。因此，把一个Panel对象到框架。
 panel = wx.Panel(window)
+#添加一个静态文本对象，以显示 ‘Hello World’在窗口内的任意位置。
 label = wx.StaticText(panel, label = "Hello World", pos = (100,100))
+#通过show()方法激活框架窗口。
 window.Show(True)
+#输入应用程序对象的主事件循环。
 app.MainLoop()
 
