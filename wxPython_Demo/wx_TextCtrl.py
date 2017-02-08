f90cf0161a2cd9ac4cd5af79644ae4a2c5012842
liuyang0618 <ly_btest@126.com>
2017-02-08 17:45:32 +0800

    TextCtrl

---
+++ wxPython_Demo/wx_TextCtrl.py
@@ -0,0 +1,42 @@
+#encoding:utf-8
+
+"""
+文本输入框详解
+"""
+import wx
+
+class myFrame(wx.Frame):
+    def __init__(self,superior):
+        wx.Frame.__init__(self, superior , -1, u"文本输入框",pos=(10,10),size=(400, 500))
+        #wx.Frame.__init__(self,superior, -1, u"示范静态文本框", size=(950,320))
+
+        #创建一个pannel
+        panel = wx.Panel(self, -1);
+
+        #创建文本
+        basicStaticLable = wx.StaticText(parent=panel,label=u"Basic Control：" )
+        #创建文本输入框
+        basicText = wx.TextCtrl(parent=panel,value=u"Python程序设计" ,size=(172,-1))
+        #默认选中
+        basicText.SetInsertionPoint(0)
+
+        #创建文本
+        pwdLabel = wx.StaticText(parent=panel,label="Password: " )
+        #创建文本输入框，并指定风格为密码
+        pwdText = wx.TextCtrl(parent=panel, value="password",size=(172,-1), style=wx.TE_PASSWORD)
+
+        #FlexGridSizer的layout布局来统一管理其中的控件布局
+        sizer = wx.FlexGridSizer(rows=2, cols=2, hgap =6, vgap=18)
+        #将控件加入到这个layout中
+        sizer.AddMany([basicStaticLable,basicText,pwdLabel,pwdText])
+        panel.SetSizer(sizer)
+
+if __name__ == "__main__":
+    #创建app对象
+    app = wx.App()
+    #创建框架类对象
+    frame = myFrame(None)
+    #框架显示
+    frame.Show(True)
+    #创建事物循环
+    app.MainLoop()
