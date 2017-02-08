e61eff6ab573a869159b87217dd0fb7bc3078a3b
liuyang0618 <ly_btest@126.com>
2017-02-08 18:47:48 +0800

    TextCtrl

--- wxPython_Demo/wx_TextCtrl.py
+++ wxPython_Demo/wx_TextCtrl.py
@@ -26,9 +26,12 @@ class myFrame(wx.Frame):
         pwdText = wx.TextCtrl(parent=panel, value="password",size=(172,-1), style=wx.TE_PASSWORD)
 
         #FlexGridSizer的layout布局来统一管理其中的控件布局
-        sizer = wx.FlexGridSizer(rows=2, cols=2, hgap =6, vgap=18)
+        #hgap: 子控件之间的水平间隔
+        #vgap:子空间之间的垂直间隔
+        sizer = wx.FlexGridSizer(rows=2, cols=2, hgap =10, vgap=18)
         #将控件加入到这个layout中
         sizer.AddMany([basicStaticLable,basicText,pwdLabel,pwdText])
+        #配置的这个panel隶属于哪个布局
         panel.SetSizer(sizer)
 
 if __name__ == "__main__":
